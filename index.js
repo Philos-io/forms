2ba0688dd8c338119b2b9df9424fe14d65b4aaf1
davy engone <davy@philos.io>
2015-08-19 02:41:15 +0200

    added asyn, custom validation and more examples

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 (function(){
 	'use strict';
 
-	angular.module('app', [])
+	angular.module('app', ['ngMessages'])
 		.controller('MainController', MainController);
 
 
-
-
 	function MainController($log){
-		
+
 		this.submit = function(){
 			$log.debug('form submitted', this);
 		};
